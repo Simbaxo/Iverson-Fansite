e865c91ee32afc85407b01500a5a5129dddd9c9d
Andres Mena <ballerlikemike23@gmail.com>
2019-03-16 12:40:26 -0400

    Convert App to React App

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BackgroundPage from './Background/BackgroundPage'
+
+const App = () => {
+  return (
+    <div>
+      <BackgroundPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Background/BackgroundPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './BackgroundPage.css'
+
+const BackgroundPage = () => {
+  return (
+    <div>
+      <ul className="slideshow">
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
+
+export default BackgroundPage
